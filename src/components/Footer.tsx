c634d81fcc38d7788734dc060935006ea7208677
Aryan Bola <aaryann5002@gmail.com>
2025-10-22 10:36:22 +0530

    added github link to it

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,8 +3,12 @@ import React from "react";
 export default function Footer() {
   return (
     <footer className="bg-gradient-to-t from-[#90ED27]/20 to-black">
-      <div className="mx-auto flex w-full max-w-4xl justify-center px-4 py-8">
+      <div className="flex w-full justify-between items-center p-8 relative">
+        <p className="text-2xl font-semibold text-lime-500 uppercase">Aryan Bola</p>
         <img src="/logo.png" className="z-10 h-20 corsor-pointer" alt="" />
+        <a href="https://github.com/Aryan-205/monster" target="_blank">
+          <img src="github-sign.png" className="w-12 h-12" alt="" />
+        </a>
       </div>
     </footer>
   );
